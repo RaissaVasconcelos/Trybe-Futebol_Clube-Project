dae3165f0140821d7b01f56d20c6a77eb4713d4f
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2022-11-25 16:33:22 -0300

    setings: Biblioteca boom.

--- app/backend/src/midlewares/login.validation.midleware.ts
+++ app/backend/src/midlewares/login.validation.midleware.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { ILogin } from '../interfaces/user.interface';
-import ErrorHttp, { HttpCode } from '../error/errorHttp';
+import ErrorHttp from '../error/errorHttp';
 import schemaUsers from '../utils/schema.joi';
+import mapError from '../error/mapError';
 
 const validationEmailPassword = (user: ILogin): void => {
   const { error } = schemaUsers.validate(user);
   console.log('error', error);
-  if (error) throw new ErrorHttp(HttpCode.BAD_REQUEST, error.message);
+  if (error) throw new ErrorHttp(mapError(error.message), error.message);
 };
 
 const validateLogin = (req: Request, _res: Response, next: NextFunction): void => {
