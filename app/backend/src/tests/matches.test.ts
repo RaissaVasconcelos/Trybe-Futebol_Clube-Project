d2da50e2c4d0433d6b33d29ac179ab017d7abde4
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2022-11-26 19:17:37 -0300

    test: Refatorando Matches.

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -6,20 +6,36 @@ import chaiHttp = require('chai-http');
 import App from '../app';
 import MatchesModel from '../database/models/MatchesModel';
 
-// import { Response } from 'superagent';
-import matches from './mocks/matches.mock.test';
-// import { array } from 'joi';
+import { matches, matchesInProgressTrue } from './mocks/matches.mock.test';
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
 
 describe('Test in Matches', () => {
   beforeEach(async () => {
     sinon.stub(MatchesModel, 'findAll')
     .resolves(matches as unknown as MatchesModel[])
   })
+
   it('Get all Matches', async () => {
-    const result = await chai.request(App).get('/matches');
+    const result = await chai.request(app).get('/matches');
 
     expect(result).to.have.status(200);
     expect(result.body).to.be.an('array');
     expect(result).to.deep.include(matches)
+  })
+
+  it('Get all Mathes in inProgress true', async () => {
+    const result = await chai.request(app).get('/matches?inProgress=true');
+
+    expect(result).to.have.status(200);
+    expect(result).to.deep.include( expect(result).to.deep.include(matchesInProgressTrue))
+  })
+
+  afterEach(() => {
+    (MatchesModel.findAll as sinon.SinonStub).restore();
   })
 })
--- app/backend/src/tests/mocks/matches.mock.test.ts
+++ app/backend/src/tests/mocks/matches.mock.test.ts
@@ -29,4 +29,42 @@ const matches = [
   }
 ]
 
-export default matches;
+const matchesInProgressTrue = [
+  {
+    id: 41,
+    homeTeam: 16,
+    homeTeamGoals: 2,
+    awayTeam: 9,
+    awayTeamGoals: 0,
+    inProgress: true,
+    teamHome: {
+      id: 16,
+      teamName: 'São Paulo'
+    },
+    teamAway: {
+      id: 9,
+      teamName: 'Internacional'
+    }
+  },
+  {
+    id: 42,
+    homeTeam: 6,
+    homeTeamGoals: 1,
+    awayTeam: 1,
+    awayTeamGoals: 0,
+    inProgress: true,
+    teamHome: {
+      id: 6,
+      teamName: 'Ferroviária'
+    },
+    teamAway: {
+      id: 1,
+      teamName: 'Avaí/Kindermann'
+    }
+  }
+]
+
+export {
+  matches,
+  matchesInProgressTrue,
+};
