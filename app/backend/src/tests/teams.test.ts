0107d50d54ac82f523f29489b4e30d8d7c174b2b
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2022-11-26 12:05:47 -0300

    test: GetById teams.

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -24,12 +24,33 @@ describe('Test in Teams', () => {
     });
     
     it('Get all teams', async () => {
-    const result = await chai.request(app).get('/teams');
-    
-    expect(result).to.have.status(200);
-    expect(result.body).to.be.an('array');
-    expect(result.body).to.deep.equal(teams);
+      const result = await chai.request(app).get('/teams');
+      
+      expect(result).to.have.status(200);
+      expect(result.body).to.be.an('array');
+      expect(result.body).to.deep.equal(teams);
   })
+
+    it('GetById team', async () => {
+      const result = await chai.request(app).get('/teams:1');
+
+      expect(result).to.have.status(200);
+      expect(result).to.deep.equal(teams[0]);
+    });
+
+    it('GetById team with id is not number', async () => {
+      const result = await chai.request(app).get('/teams:a');
+
+      expect(result).to.have.status(400);
+      expect(result).to.deep.equal('Id is number');
+    })
+
+    it('GetById team id not found', async () => {
+      const result = await chai.request(app).get('/teams:32');
+
+      expect(result).to.have.status(404);
+      expect(result).to.deep.equal('Team is not found');
+    })
 
   afterEach(()=>{
     (TeamsModel.findAll as sinon.SinonStub).restore();
